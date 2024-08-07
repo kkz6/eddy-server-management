6f431978d859f4da997c7b9f6ac8edda441d499d
Karthick K <karthickk1996@hotmail.com>
2024-08-07 15:52:55 +0530

    callback actions

--- resources/views/tasks/common-functions.blade.php
+++ resources/views/tasks/common-functions.blade.php
@@ -1,14 +1,17 @@
 # Send a POST request to the given URL, ignoring the response and errors
 function httpPostSilently()
 {
+    log_file="callback.log"
+
     if [ -z "${2:-}" ]; then
-        (curl -X POST --silent --max-time 15 --output /dev/null $1 || true)
+        (curl -X POST --silent --max-time 15 $1 -o /dev/null 2>>$log_file || true)
     else
-        (curl -X POST --silent --max-time 15 --output /dev/null $1 -H 'Content-Type: application/json' --data $2 || true)
+        (curl -X POST --silent --max-time 15 $1 -H 'Content-Type: application/json' --data $2 -o /dev/null 2>>$log_file || true)
     fi
 }
 
 function httpPostRawSilently()
 {
-    (curl -X POST --silent --max-time 15 --output /dev/null $1 --data "$2" || true)
+    log_file="callback.log"
+    (curl -X POST --silent --max-time 15 $1 --data "$2" -o /dev/null 2>>$log_file || true)
 }
